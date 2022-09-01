d783ede6d913da43561e5063f32c82dfad244bdb
AlexPuchenkov <alex12121414@mail.ru>
2022-09-01 13:43:18 +0300

    add greetings and farewell

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import csv
 
-with open('.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+with open('test_data.csv', 'r', encoding='utf-8', newline='') as File:
+    reader = csv.reader(File)
+    for row in reader:
+        text_message = "".join(row)
+        greetings = True if 'Здравствуйте' in text_message else False
+        farewell = True if 'досвидания' in text_message else False
+        print(text_message, greetings, farewell)
+
