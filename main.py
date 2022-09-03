23a8e21df6f45561c6b19df44d4ced2dbb509e12
AlexPuchenkov <alex12121414@mail.ru>
2022-09-03 09:20:35 +0300

    self_presentation_all and company_name_all

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import csv
 
 greetings_all = ['здравствуйте', 'здраствуй', 'здрасте', 'приветствую']
 farewell_all = ['досвидания', 'до встречи', 'всего доброго', 'всего хорошего']
+self_presentation_all = ['меня зовут', 'моё имя']
+company_name_all = ['представляю', 'компания', 'компанию', 'компании', 'организацию']
 
 
 def main():
@@ -10,33 +12,46 @@ def main():
         for row in reader:
             text_message = "".join(row)
             text_message = text_message.lower()
+
             for greet in greetings_all:
                 if greet in text_message:
                     greetings = True
                     break
                 greetings = False
+
             for far in farewell_all:
                 if far in text_message:
                     farewell = True
                     break
                 farewell = False
-            # greetings = True if 'здравствуйте' in text_message else False
-            # farewell = True if 'досвидания' in text_message else False
+
+            for self in self_presentation_all:
+                if self in text_message:
+                    self_presentation = self
+                    break
+                self_presentation = False
+
+            for name in self_presentation_all:
+                if name in text_message:
+                    manager_name = text_message.partition(name)[2].split()[0]
+                    break
+                manager_name = False
+
+            for name in company_name_all:
+                if name in text_message:
+                    company_name = text_message.partition(name)[2].split()[0]
+                    break
+                company_name = False
+
             for far in farewell_all:
                 if far in text_message:
-                    farewell = True
+                    said_goodbye = far
                     break
-                farewell = False
-            self_presentation = 'меня зовут' if 'меня зовут' in text_message else False
-            manager_name = text_message.partition('меня зовут')[2].split()[0] if 'меня зовут' in text_message else False
-            company_name = text_message.partition('компании')[2].split()[0] if 'компании' in text_message else False
-            said_goodbye = text_message.partition('досвидания')[2].split()[0] if 'досвидания' in text_message else False
+                said_goodbye = False
+
             print('text_message = ', text_message, '\n greetings = ', greetings, '\n self_presentation = ',
                   self_presentation, '\n manager_name = ', manager_name, '\n company_name = ',
                   company_name, '\n said_goodbye = ', said_goodbye, '\n farewell = ', farewell)
-            # print(text_message, text_message.count(' ') + 1)
-            # print(text_message[text_message.find(a)+1:])
-            # print(text_message.partition('меня зовут')[2].split()[0])
 
 
 if __name__ == '__main__':
